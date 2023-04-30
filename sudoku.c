5e86398c0ebcb460832dd7ca3e1f1568bbfc6407
Replit user <>
2023-04-30 19:59:39 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -58,7 +58,7 @@ List* get_adj_nodes(Node* n){
         {
             if (nAux->sudo[i][j] == 0)
             {
-                n->sudo[i][j] = 1;
+                nAux->sudo[i][j] = 1;
                 break;
             }
         }
