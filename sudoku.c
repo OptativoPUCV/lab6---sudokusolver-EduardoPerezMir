293a1cd730352b0b567d1508d623ba7d82e8e475
Replit user <>
2023-05-02 17:44:49 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -147,7 +147,7 @@ Node* DFS(Node* initial, int* cont){
     while (is_empty(pila))
     {
         nAux = first(pila);
-        popFront(pila);
+        pop(pila);
         
         if (is_final(nAux))
         {
